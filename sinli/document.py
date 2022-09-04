bd3bf7141897d335e825b4d53b503dc6727ac104
raneq <raneq@posteo.net>
2022-09-04 04:14:25 +0200

    WIP Implement a hier. structure top-down
    - Document will be subclassed by each type of document, and these will implement the parsing of specific lines
    - Line is a generic, simple class with just one common subclass. Other line subclasses (if needed) will be defined inside each document subclass
    - To route the parsing and to define each specific case,  we'll use enums. In document, the name is also the encoded text representation. The value is the human description in the specs. In line, enums define the fields. As they can be hashed, we can later use them to build a dict of fields with the actual read values, and this same array can be used to reconstruct the original string, as it contains all the needed data to do so

--- sinli/document.py
+++ sinli/document.py
@@ -1,37 +1,49 @@
 from io import open
+import os
 
 class Document:
-    def __init__(self, id_line: IdentificationLine, header_line: HeaderLine, detail_lines: [DetailLine], other_lines: [GenericLine]):
-        self.id_line = id_line
-        self.header_line = header_line
-        self.detail_lines = detail_lines
-        self.other_lines = other_lines
+    def __init__(self):
+        self.id_line: IdentificationLine = None
+        self.header_line: Line = None
+        self.detail_lines: [Line] = []
+        self.other_lines: [Line] = []
+
+    def read_header_line(self, line: str) -> Line:
+        raise NotImplementedError("Depends on document type. Implement in subclasses")
+
+    def read_detail_line(self, line: str) -> Line:
+        raise NotImplementedError("Depends on document type. Implement in subclasses")
+
+    def read_other_line(self, line: str) -> Line:
+        raise NotImplementedError("Depends on document type. Implement in subclasses")
+
+    class Type(Enum):
+        ABONO  = "Albarán o Factura de Abono"
+        CAMPRE = "Cambios de precio"
+        ESTADO = "Cambios de estado"
+        LIBROS = "Ficha del Libro"
+        MENSAJ = "Mensaje"
 
     def process_line(self, line: str):
         l = LineReader(line)
         tdoc = l.read(1)
         if tdoc == "I": # Identification
-            pass
+            self.id_line = IdentificationLine(line)
         elif tdoc == "C": # header (Cabecera)
-            pass
+            self.header_line = self.read_header_line(line)
         elif tdoc == "D": # Detail
-            pass
-        #elif tdoc in ["T", "V", "R", "M", "E", "P", "H"]: # other
+            self.detail_lines.append(self.read_detail_line(line))
         # other #
-        elif tdoc == "T": # Totals
-            pass
-        elif tdoc == "V": # tax (IVA)
-            pass
-        elif tdoc == "R": # Refusal (Rechazo)
-            pass
-        elif tdoc == "M": # Message
-            pass
-        elif tdoc == "E": # status (Estado)
-            pass
-        elif tdoc == "P": # Payment time (vencimiento)
-            pass
-        elif tdoc == "H": # dropsHiping (entregas directas de distribuidoras a cliente final en nombre de la librería que recibe el pedido)
-            pass
+        elif (
+            tdoc == "T" or   # Totals
+            tdoc == "V" or   # tax (IVA)
+            tdoc == "R" or   # Refusal (Rechazo)
+            tdoc == "M" or   # Message
+            tdoc == "E" or   # status (Estado)
+            tdoc == "P" or   # Payment time (vencimiento)
+            tdoc == "H"      # dropsHiping (entregas directas de distribuidoras a cliente final en nombre de la librería que recibe el pedido)
+        ):
+            self.other_lines.append(self.read_other_line(line))
         else: # error
             raise SyntaxError("SINLI syntax error", f"El codi de registre {tdoc} no es reconeix")
 
@@ -49,4 +61,13 @@ class Document:
         with open(filename, encoding="cp850") as f:
             for line in f:
                 self.process_line(line)
+
+    def __str__(self) -> str:
+        slines = []
+        slines.append(str(self.id_line))
+        slines.append(str(self.header_line))
+        if len(self.detail_lines > 0): slines.append(os.linesep.join([str(line) for line in self.detail_lines]))
+        if len(self.other_lines > 0):  slines.append(os.linesep.join([str(line) for line in self.other_lines]))
+        return os.linesep.join(slines)
+
 
--- sinli/line.py
+++ sinli/line.py
@@ -1,5 +1,35 @@
 class Line:
     def __init__(self):
-        self.
-    def from_str():
+        pass
+
+    class Field(Enum):
+        EXAMPLE: (1, 7, "Example field located in 1st position with length 7")
+
+    def load_from_dict(self, fields: {}) -> self:
+        raise NotImplementedError("Depends on line type. Implement in subclasses")
+
+    def __str__(self) -> str:
+        raise NotImplementedError("Depends on line type. Implement in subclasses")
+
+    def from_str(self, line: str, field_enum: Enum):
+        line_dict = {}
+        for field in field_enum:
+            line_dict[field] = line[field.value[0], field.value[0]+field.value[1]].strip()
+        return self.from_dict()
+
+# TODO consider converting to a @dataclass
+class IdentificationLine(Line):
+    def __init__(self):
+        self.from_email = ""
+        self.to_email = ""
+        self.version = 0
+
+    #TODO def load_from_dict / either method or from_dict as a @classmethod
+
+    class Field(Enum):
+        FROM =    (1, 50, "E-mail origen")
+        TO =      (51, 50, "E-mail destino")
+        DOCTYPE = (101, 6, "Tipo de Fichero")
+        VERSION = (107, 2, "Versión fichero")
+
 
